102d956a54f45adae5b19e2187ad456e9130dae5
lolaakhmedova <0404lola@akhmedova>
2026-09-20 13:00:15 -0400

    added print message for Git practice

--- src/week06/GitTestClass.java
+++ src/week06/GitTestClass.java
@@ -3,5 +3,6 @@ package week06;
 public class GitTestClass {
     static void main() {
         System.out.println( "Hello Git");
+        System.out.println("First Change: practicing Git commit ");
     }
 }
